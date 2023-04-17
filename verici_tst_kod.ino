d19a3e65b6ec052a6674ae79bad556fa324eaafd
THUNDER <72344723+alisabourii@users.noreply.github.com>
2023-04-17 18:48:03 +0300

    V3

--- verici_tst_kod.ino
+++ verici_tst_kod.ino
@@ -2,13 +2,20 @@
 #include <nRF24L01.h>
 #include <SPI.h>
 
+#include <MPU6050_tockn.h>
+#include <Wire.h>
+
+MPU6050 mpu6050(Wire);
+
 Enrf24 nRF(9, 10, 3);  // CE , CSN/CS/SS , IRQ bacaklarının Arduino ile bağlantıları tanımlandı
 
 // verici adresi tanımlandı aynı adres alıcıda da tanımlanmalıdır
 const byte verici_adresi[] = { 0xDE, 0xAD, 0xBE, 0x0F, 0x01 };
 
-int ac_kapat=0; // alıcının LED'i kapatıp açmak için gerekli bir değişken tanımlandı
+
 int BUTON=4;
+
+long timer = 0;
 
 void setup() 
 {
@@ -18,27 +25,37 @@ void setup()
   SPI.begin(); // SPI başlat
   SPI.setDataMode(SPI_MODE0); // SPI MODE0 seçildi, nrf24l01 MODE0 ile iletişim kurmaktadır
   SPI.setBitOrder(MSBFIRST); // bit sıralaması MSB'den LSB'ye doğru ayarlandı
-  
-  // datarate 250000/1000000/2000000, channel 0/125
-  // 1000000 datarate seçildi , 124 nolu kanal seçildi
   nRF.begin(1000000,124);  
-
   nRF.setTXaddress(verici_adresi); // verici adresi ayarlandı
+
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true); 
 }
 
 void loop() 
 {
+  mpu6050.update();
+  if(millis() - timer > 1000){
+      int FB = mpu6050.getAngleY();
+      int RL = mpu6050.getAngleX();
 
-  if(digitalRead(BUTON) == 0){
-    Serial.println("A");
-    nRF.print("A"); 
-    nRF.flush(); 
-  }
-  else{
-     Serial.println("K");
-     nRF.print("K"); 
-     nRF.flush();  
-  }
-   delay(200); 
-  
+      Serial.println(FB);
+      if(FB < -5){
+        Serial.println("F");
+        nRF.print("F"); 
+        nRF.flush(); 
+          }
+      else if(FB > 20){
+        Serial.println("B");
+        nRF.print("B"); 
+        nRF.flush(); 
+          }
+      else if(FB >= -5 && FB <= 20){
+        Serial.println("M");
+        nRF.print("M"); 
+        nRF.flush(); 
+          }                
+    timer = millis();
+  } 
 }
